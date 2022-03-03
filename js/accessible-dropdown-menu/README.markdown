1e58b643e9dcb039c8087daee6debd799122c76f
Jakub Raus <jakub.raus@zoner.com>
2022-03-03 11:04:56 +0100

    Init Commit

---
+++ js/accessible-dropdown-menu/README.markdown
@@ -0,0 +1,16 @@
+# Accessible Menu jQuery plugin
+
+## Doc
+
+https://hotgeart.github.io/accessible-menu/
+
+## How to
+
+1. Take the accessible-menu.js
+2. Add this code
+
+```javascript
+    $("#my-menu").accessibleMenu();
+```
+
+![ez](ez.gif)
---
+++ js/custom-demobanner.js
@@ -0,0 +1,39 @@
+  $(document).ready(function() {
+    demoBannerToggle();
+  });
+
+  $(window).on('resize', function() {
+    demoBannerToggle();
+  });
+
+  // vypocet realne vysky stranky, jelikoz
+  // #page-wrapper je kvuli parallaxu nastaven na 100vh
+  // -> soucet vysek vnitrnich elementu #page-wrapper
+  function calcPageHeight() {
+    var totalHeight = 0;
+    $("#page-wrapper > *").each(function() {
+      totalHeight += $(this).outerHeight();
+    });
+    return totalHeight;
+  }
+
+  // schovavani demo banneru ("Toto je vzorovy web")
+  // na konci stranky, aby neprekryval paticku
+  function demoBannerToggle() {
+    var $pageWrapper = $("#page-wrapper");
+    var wrapperHeight = $($pageWrapper).outerHeight();
+    var $demoBanner = $("#demoBanner");
+    var bannerHeight = $($demoBanner).outerHeight();
+    var pageHeight = calcPageHeight();
+
+    $($pageWrapper).scroll(function() {
+      var pageOffset = $($pageWrapper).scrollTop();
+      
+      if ( (pageOffset + wrapperHeight + bannerHeight) > (pageHeight) ) {
+        $($demoBanner).removeClass("active");
+      } else {
+        $($demoBanner).addClass("active");
+      }
+    });
+  } 
+
